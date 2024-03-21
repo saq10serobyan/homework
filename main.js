978f33eb0ad19fe74962656547ca3312ab7585c7
saq10serobyan <155264926+saq10serobyan@users.noreply.github.com>
2024-03-21 15:31:24 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,93 +1,204 @@
-// //task1
 
-function find(arr,cb){
-    for(let i = 0; i < arr.length;i++){
-        if(cb(arr[i])){
-            return arr[i]
-        }
+//task1
+
+class Author {
+    constructor(name, email, gender) {
+        this.name = name
+        this.email = email
+        this.gender = gender
     }
-    return undefined
+    get n() {
+        return this.name
+    }
+    get e() {
+        return this.email
+    }
+    get gend() {
+        return this.gender
+    }
+    toString() {
+        return 'this is toString method in Author class'
+    }
 }
+let a = new Author('saq', 'ssa', 'male')
 
 
-
+class Book {
+    constructor(title, author, price, quantity) {
+        this.title = title
+        this.author = author
+        this.price = price
+        this.quantity = quantity
+    }
+    get tit() {
+        return this._title
+    }
+    set tit(v) {
+        this._title = v
+    }
+    get aut() {
+        return this._author
+    }
+    set aut(v) {
+        this._author = v
+    } get pr() {
+        return this._price
+    }
+    set pr(v) {
+        this._price = v
+    } get quant() {
+        return this._quantity
+    }
+    set quant(v) {
+        this._quantity = v
+    }
+    getProfit(price, quantity) {
+        return price * quantity
+    }
+    toString() {
+        return 'this is tostring method in book class'
+    }
+}
+let b = new Book('EL', a, 200, 20)
 
 
 //task2
 
-function filter(arr,cb){
-    let res = []
-    for(let i = 0; i < arr.length; i++){
-        if(cb(arr[i])){
-            res.push(arr[i])
+class Account {
+    constructor(id, name, balance) {
+        this.id = id
+        this.name = name
+        this.balance = balance
+    }
+    static compare(ac1, ac2) {
+        for (const key in ac1) {
+            if (!(ac1[key] === ac2[key])) {
+                return 'false'
+            }
+        }
+        return true
+    }
+    get i() {
+        return this.id
+    }
+    get n() {
+        return this._name
+    }
+    set n(v) {
+        this._name = v
+    }
+    get b() {
+        return this._balance
+    }
+    set b(v) {
+        this._balance = v
+    }
+    credit(amaount) {
+        this.balance += amaount
+        return this.balance
+    }
+    debit(amaount) {
+
+        if (this.balance > amaount) {
+            this.balance -= amaount
+            return `your balance now is ${this.balance}`
+        } else {
+            return 'Amount exceeded balance.'
         }
     }
-    return res
-}
-
-
-
-
-//task3
-
-function map(arr,cb){
-    let res = []
-    for(let i = 0; i < arr.length; i++){
-        res.push(cb(arr[i]))
-    }
-    return res
-}
-
-
-
-
-
-//task4
-
-function every(arr,cb){
-    for(let i = 0; i < arr.length; i++){
-        if(!cb(arr[i])){
-            return false
+    transferTo(anotherAcc, amaount) {
+        if (this.balance > amaount) {
+            anotherAcc.balance += amaount
+            this.balance -= amaount
+            return this.balance
+        } else {
+            return 'Amount exceeded balance.'
         }
     }
-    return true
+    toString() {
+        return 'this is tostring method iN Account class'
+    }
 }
 
+let c = new Account(12, 'saq', 2311)
+let d = new Account(12, 'saq', 231)
 
 
+//task 3
 
-//task5
+class Person {
+    constructor(firstName, lastName, gender, age) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.gender = gender
+        this.age = age
+    }
+    get fn(){
+        return this._firstName
+    }
+    set fn(v){
+        this._firstName = v
+    }
+    get ln(){
+        return this._lastName
+    }
+    set ln(v){
+        this._lastName = v
+    }
+    get gen(){
+        return this._gender
+    }
+    set gen(v){
+        this._gender = v
+    }
+    get ag(){
+        return this._age
+    }
+    set ag(v){
+        this._age = v
+    }
+    toString(){
+        return 'this is tostring method in person class'
+    }
+}
 
-function some(arr,cb){
-    for(let i = 0; i < arr.length;i++){
-        if(cb(arr[i])){
-            return true
+class Student extends Person{
+    constructor(array,year, fee){
+        this.programName = array[0]
+        this.grade = array[1]
+        this.year = year
+        this.fee = fee
+    }
+    get pn(){
+        return this._programName
+    }
+    set pn(v){
+        this._programName = v
+    }
+    get gr(){
+        return this._grade
+    }
+    set gr(v){
+        this._grade = v
+    }
+    get y(){
+        return this._year
+    }
+    set y(v){
+        this._year = v
+    }
+    get f(){
+        return this._fee    
+    }
+    set f(v){
+        this._fee = v
+    }
+    passExam(programName,grade){
+        if(grade === 'great' || grade === 50){
+            this.year += 1
         }
     }
-    return false
-}
-
-
-
-
-//task6
-
-function findIndex(arr,cb){
-    for(let i = 0; i < arr.length; i++){
-        if(cb(arr[i])){
-            return arr.indexOf(arr[i])
-        }
-    }
-    return -1
-}
-
-
-
-//task7 
-
-function forEach(arr,cb){
-    for(let i = 0; i < arr.length; i++){
-        cb(arr[i])
+    toString(){
+        return 'this is tostring method in student class'
     }
 }
-
